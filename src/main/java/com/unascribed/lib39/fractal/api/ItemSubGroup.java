679f9f2fc5f52e0798d125ed7ed4dae4c9c39b0d
Una Thompson <me@unascribed.com>
2022-08-08 00:13:02 -0700

    Don't use Unsafe for item subgroups

--- src/main/java/com/unascribed/lib39/fractal/api/ItemSubGroup.java
+++ src/main/java/com/unascribed/lib39/fractal/api/ItemSubGroup.java
@@ -1,79 +1,37 @@
 package com.unascribed.lib39.fractal.api;
 
-import java.util.List;
-
-import com.google.gson.internal.UnsafeAllocator;
 import com.unascribed.lib39.fractal.quack.ItemGroupParent;
 
-import com.google.common.collect.Lists;
-
-import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.collection.DefaultedList;
 
 public class ItemSubGroup extends ItemGroup {
 
-	private int index;
-	private String id;
 	private ItemGroup parent;
-	private Text translationKey;
-	private List<ItemStack> additionalStacks;
+	private int indexInParent;
 	
-	private ItemSubGroup() {
-		// NOT CALLED
-		super(-1, null);
-	}
-	
-	public static ItemSubGroup create(ItemGroup parent, Identifier id) {
-		try {
-			ItemGroupParent igp = (ItemGroupParent)parent;
-			ItemSubGroup isg = UnsafeAllocator.create().newInstance(ItemSubGroup.class);
-			isg.index = igp.lib39Fractal$getChildren().size();
-			isg.id = id.getNamespace()+"."+id.getPath();
-			isg.parent = parent;
-			isg.translationKey = Text.translatable("itemGroup."+isg.id);
-			isg.additionalStacks = Lists.newArrayList();
-			igp.lib39Fractal$getChildren().add(isg);
-			if (igp.lib39Fractal$getSelectedChild() == null) {
-				igp.lib39Fractal$setSelectedChild(isg);
-			}
-			return isg;
-		} catch (Exception e) {
-			throw new Error(e);
+	private ItemSubGroup(ItemGroup parent, Identifier id) {
+		super(0, id.getNamespace()+"."+id.getPath());
+		this.parent = parent;
+		ItemGroupParent igp = (ItemGroupParent)parent;
+		this.indexInParent = igp.lib39Fractal$getChildren().size();
+		igp.lib39Fractal$getChildren().add(this);
+		if (igp.lib39Fractal$getSelectedChild() == null) {
+			igp.lib39Fractal$setSelectedChild(this);
 		}
 	}
 	
-	@Override
-	public void appendStacks(DefaultedList<ItemStack> stacks) {
-		super.appendStacks(stacks);
-		stacks.addAll(additionalStacks);
-	}
-	
-	@Override
-	public int getIndex() {
-		return index;
-	}
-	
-	@Override
-	public String getName() {
-		return id;
+	public static ItemSubGroup create(ItemGroup parent, Identifier id) {
+		return new ItemSubGroup(parent, id);
 	}
 	
 	public ItemGroup getParent() {
 		return parent;
 	}
-
-	@Override
-	public EnchantmentTarget[] getEnchantments() {
-		return new EnchantmentTarget[0];
-	}
 	
-	@Override
-	public Text getTranslationKey() {
-		return translationKey;
+	public int getIndexInParent() {
+		return indexInParent;
 	}
 	
 	@Override
--- src/main/java/com/unascribed/lib39/fractal/mixin/MixinItemGroup.java
+++ src/main/java/com/unascribed/lib39/fractal/mixin/MixinItemGroup.java
@@ -2,8 +2,12 @@ package com.unascribed.lib39.fractal.mixin;
 
 import java.util.List;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -21,7 +25,7 @@ public class MixinItemGroup implements ItemGroupParent {
 
 	private final List<ItemSubGroup> lib39Fractal$children = Lists.newArrayList();
 	private ItemSubGroup lib39Fractal$selectedChild = null;
-	
+
 	@Inject(at=@At("HEAD"), method="appendStacks", cancellable=true)
 	public void appendStacksHead(DefaultedList<ItemStack> stacks, CallbackInfo ci) {
 		if (lib39Fractal$selectedChild != null) {
@@ -52,6 +56,30 @@ public class MixinItemGroup implements ItemGroupParent {
 	@Override
 	public void lib39Fractal$setSelectedChild(ItemSubGroup group) {
 		lib39Fractal$selectedChild = group;
+	}
+
+	// from Polymer
+	private static ItemGroup[] LIB39FRACTAL$GROUPS_OLD;
+	@Mutable @Shadow @Final
+	public static ItemGroup[] GROUPS;
+
+	@Inject(at=@At(value="FIELD", target="net/minecraft/item/ItemGroup.GROUPS:[Lnet/minecraft/item/ItemGroup;", shift=Shift.BEFORE),
+			method="<init>")
+	private void lib39Fractal$replaceArrayToSkipAdding(int index, String id, CallbackInfo ci) {
+		Object self = this;
+		if (self instanceof ItemSubGroup) {
+			LIB39FRACTAL$GROUPS_OLD = GROUPS;
+			GROUPS = new ItemGroup[1];
+		}
+	}
+
+	@Inject(at=@At("TAIL"),
+			method="<init>")
+	private void lib39Fractal$unreplaceArray(int index, String id, CallbackInfo ci) {
+		Object self = this;
+		if (self instanceof ItemSubGroup) {
+			GROUPS = LIB39FRACTAL$GROUPS_OLD;
+		}
 	}
 	
 }
